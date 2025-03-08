f320665e84e8653bfe139071f99b32c38f05d585
Gokcenaz Torgan <gokcetorgan@gmail.com>
2025-03-08 22:09:56 +0300

    added frontend files

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARE
+app.use(express.static('public'));
+
+app.get('/addphoto', (req, res) => {
+  res.render('addphoto');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
 app.get('/', (req, res) => {
-  const magazine = {
-    id: 1,
-    title: 'Magazine title',
-    description: 'Magazine description',
-  };
-  res.send(magazine);
+  res.render('index');
 });
 
 const PORT = 5000;
